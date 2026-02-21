3e2ba03861bcadeb7506961b08d1150e1085d208
Andrea Gentili <andrea.gentili@gmail.com>
2026-02-21 09:54:23 +0100

    Adding menu

--- scenes/gameplay_screen.gd
+++ scenes/gameplay_screen.gd
@@ -1,2 +1,5 @@
 extends Node2D
 class_name GameplayScreen
+
+func start(run_config: RunConfiguration) -> void:
+  prints("Starting gameplay with config: ", run_config)
--- scenes/main.gd
+++ scenes/main.gd
@@ -12,15 +12,49 @@ class_name Main
 var current_run_config: RunConfiguration
 
 @export var main_menu_scene: PackedScene
+@export var gameplay_screen_scene: PackedScene
+
+# Private variables -------
+var _main_menu: MainMenu
+var _gameplay_screen: GameplayScreen
 
 func _ready() -> void:
-  var main_menu = main_menu_scene.instantiate()
-  add_child(main_menu)
-  main_menu.start_pressed.connect(_on_main_menu_start_pressed)
-  main_menu.quit_pressed.connect(_on_main_menu_quit_pressed)
+  _show_main_menu()
+
+# Main menu ---------------
+
+func _show_main_menu() -> void:
+  if _main_menu:
+    remove_child(_main_menu)
+    _main_menu.queue_free()
+    
+  if _gameplay_screen:
+    remove_child(_gameplay_screen)
+    _gameplay_screen.queue_free()
+
+  _main_menu = main_menu_scene.instantiate()
+  add_child(_main_menu)
+  _main_menu.start_pressed.connect(_on_main_menu_start_pressed)
+  _main_menu.quit_pressed.connect(_on_main_menu_quit_pressed)
 
 func _on_main_menu_start_pressed() -> void:
   current_run_config = RunConfiguration.new()
+  _show_gameplay_screen()
+  _gameplay_screen.start(current_run_config)
 
 func _on_main_menu_quit_pressed() -> void:
   get_tree().quit()
+
+# Gameplay screen ----------
+
+func _show_gameplay_screen() -> void:
+  if _main_menu:
+    remove_child(_main_menu)
+    _main_menu.queue_free()
+
+  if _gameplay_screen:
+    remove_child(_gameplay_screen)
+    _gameplay_screen.queue_free()
+
+  _gameplay_screen = gameplay_screen_scene.instantiate()
+  add_child(_gameplay_screen)
